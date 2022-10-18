75cb00ab2d41c34ab062aa8ae11396532b93002c
yoshiaki.n <52188314+user4460@users.noreply.github.com>
2022-10-18 10:35:32 +0800

    todos

--- bktodogo/todos/list.go
+++ bktodogo/todos/list.go
@@ -1,1 +1,15 @@
-//
+//todoリストを表示する
+func List() {
+	// ファイルを開く
+	file, err := os.OpenFile("todos.txt", os.O_RDONLY, 0)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	// ファイルの内容を読み込む
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+}
